7ef4005aa442c6dc8cdc148edf8f6767a690cd29
wley3337 <wley3337@gmail.com>
2019-11-01 17:32:53 -0400

    started implimenting the form. Doesn't seem to render the inputs

--- frontend/codealong/src/App.vue
+++ frontend/codealong/src/App.vue
@@ -1,17 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div>
+    <h1 class="title">Calculator</h1>
   </div>
+  <hr>
+  <div>
+    <b-container fluid>
+      <b-row class="text-center">
+        <b-col cols="2" class="text-center"></b-col>
+        <b-col cols="4" class="text-center">
+          <form>
+            <div class="field">
+              <label class="lable">First Number</label>
+              <b-form-input name="num1" v-model="num1" v-validate="'required|digits'" class="input" type="text"></b-form-input>
+            </div>
+            <div class="field" style="margin-top:12px;">
+              <label class="label">Second Number</label>
+              <b-form-input name="num2" v-model="num2" class="input" v-validate="'required|digits'" type="text"></b-form-input>
+            </div>
+          </form>
+        </b-col>
+        <b-col cols="4" class="text-left">
+          <div><label class="label">Addition: {{ add }}</label></div>
+          <div><label class="lable">Multiplication: {{ mul }}</label></div>
+          <div><label class="label">Subtraction: {{ sub }}</label></div>
+          <div><label class="label">Division: {{ div }}</label></div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+
+  &nbsp;
+  <div>
+    <b-button variant="primary" v-on:click="postreq()">Calculate</b-button>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+ 
   }
 }
 </script>
